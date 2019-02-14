516b8fa612727e0aaf3cfa00d46e2d415250c260
quentin.ravet <quentin.ravet@yahoo.fr>
2019-02-14 11:13:49 +0100

    Ajout picto + construction footer 14/02

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,23 @@
 <footer class="columns">
-
+    <div class="container grid-xl">
+        <div class="columns">
+            <div class="column col-6">
+                <h3 class="text-uppercase">restez connectés :</h3>
+                <p>Recevez nos bons plans par e-mail grâce à notre newsletter mensuelle.</p>
+                <div class="columns">
+                    <div class="column col-6">
+                        <input type="text" name="email" placeholder="...Votre e-mail">
+                    </div>
+                    <div class="column col-6">
+                        <button class="text-uppercase" type="submit">Envoyer</button>
+                    </div>
+                </div>
+                <div class="columns">
+                    
+                </div>
+            </div>
+            <div class="column col-4"></div>
+            <div class="column col-2"></div>
+        </div>
+    </div>
 </footer>
