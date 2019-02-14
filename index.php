2188c64f91c06241b2b893f8726b6bf1d2144624
quentin.ravet <quentin.ravet@yahoo.fr>
2019-02-14 09:24:24 +0100

    début projet BSM

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <!-- Import FrameWork SPECTRE -->
+    <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre-exp.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre-icons.min.css">
+
+</head>
+<body>
+
+    <div class="container">
+        <div class="columns">
+            <div class="column col-12">
+                <?php include './inc/footer.php' ?>                
+            </div>
+        </div>
+    </div>
+    
+    <!-- Import JQUERY -->
+    <script
+    src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous">
+    </script><!-- Fin d'import JQUERY -->
+</body>
+</html>
