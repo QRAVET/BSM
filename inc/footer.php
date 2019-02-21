d3e4802b2b91b7457567bcda02208c3c6620170e
quentin.ravet <quentin.ravet@yahoo.fr>
2019-02-21 08:45:37 +0100

    démarrage

--- inc/footer.php
+++ inc/footer.php
@@ -1,14 +1,13 @@
 <footer class="columns">
     <div class="container grid-xl">
         <div class="columns">
-            <div class="column col-6">
+            <div class="column col-5">
                 <h3 class="text-uppercase">restez connectés :</h3>
                 <p>Recevez nos bons plans par e-mail grâce à notre newsletter mensuelle.</p>
                 <div class="columns">
-                    <div class="column col-5">
+                    <div class="column col-6">
                         <input type="text" name="email" placeholder="...Votre e-mail">
                     </div>
-                    <div class="column col-1"></div>
                     <div class="column col-6">
                         <button class="text-uppercase" type="submit">Envoyer</button>
                     </div>
@@ -16,10 +15,10 @@
                 <div class="columns">
                     <div class="logo-fb-blue"></div>
                     <div class="logo-insta-blue"></div>
-
                 </div>
             </div>
-            <div class="column col-4"></div>
+            <div class="column col-1"></div>
+            <div class="column col-3"></div>
             <div class="column col-2"></div>
         </div>
     </div>
