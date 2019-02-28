d18e2d55ec42106dc707d61b0a246c33de727a08
quentin.ravet <quentin.ravet@yahoo.fr>
2019-02-28 11:32:33 +0100

    fin de matinée 28.02

--- inc/footer.php
+++ inc/footer.php
@@ -2,7 +2,7 @@
     <div class="container grid-xl ">
         <div class="columns">
             <div class="column col-5">
-                <h3 class="text-uppercase">restez connectés :</h3>
+                <h3 class="text-uppercase text-bold blue-green">restez connectés :</h3>
                 <p>Recevez nos bons plans par e-mail grâce à notre newsletter mensuelle.</p>
                 <div class="columns">
                     <div class="column col-6">
@@ -15,7 +15,7 @@
             </div>
             <div class="column col-1"></div>
             <div class="column col-3">
-                <h3 class="text-uppercase">navigation :</h3>
+                <h3 class="text-uppercase text-bold blue-green">navigation :</h3>
                 <div class="menu-footer">
                     <p>Accueil</p>
                     <p>La Base sous-marine</p>
@@ -26,12 +26,25 @@
             </div>
             <div class="column col-1"></div>
             <div class="column col-2">
-                <div class="button-white col-mx-auto"></div>
+                <div class="button-white-up col-mx-auto"></div>
                 <p class="text-center">Haut de page</p>
             </div>
         </div>
         <div class="columns">
-            <div class="column col-9"></div>
+            <div class="column col-9">
+                <div class="divider"></div>
+                <div class="text-center">
+                    <p>Bassins à flot - Bacalan - Boulevard Alfred Daney 33000 BORDEAUX - tél : 05.56.11.11.50</p>
+                </div>
+                <div class="columns">
+                    <div class="column col-6 col-mx-auto">
+                        <div class="divider"></div>
+                    </div>
+                </div>
+                <div class="text-center">
+                    <p>&#x24B8;LaBaseSousMarine_2019 <a href="mentions-legales.php">Mentions légales</a></p>
+                </div>
+            </div>
             <div class="column col-1"></div>
             <div class="column col-2">
                 <div class="logo-fb-blue col-mx-auto"></div>
